dd71bde11f0851ab56426adcbf5f2de130695de9
olvr-me <resing.oliver@gmail.com>
2023-04-01 19:59:37 +0200

    Added song skeleton component

--- app/[date]/loading.tsx
+++ app/[date]/loading.tsx
@@ -1,9 +1,7 @@
+import SongSkeleton from "@/components/SongSkeleton";
+
 export default async function Page({ params }) {
     return (
-        <>
-            <div className="animate-pulse w-600 h-600 bg-slate-500" />
-            <h3>loading...</h3>
-            <h3 className='text-slate-400'>loading...</h3>
-        </>
+        <SongSkeleton />
     )
 }
---
+++ components/SongSkeleton.tsx
@@ -0,0 +1,9 @@
+export default function SongSkeleton() {
+    return (
+        <>
+            <div className="animate-pulse w-600 h-600 bg-slate-500" />
+            <h3>loading...</h3>
+            <h3 className='text-slate-400'>loading...</h3>
+        </>
+    );
+};
