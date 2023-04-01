6706ce9fd0948f85305d9c4b674cb5a19247837f
olvr-me <resing.oliver@gmail.com>
2023-04-01 19:48:01 +0200

    Added skelton loader for image component

---
+++ app/[date]/loading.tsx
@@ -0,0 +1,5 @@
+export default async function Page({ params }) {
+    return (
+        <div className="animate-pulse w-600 h-600 bg-slate-700" />
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,14 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '600': '600px',
+      },
+      height: {
+        '600': '600px'
+      }
+    },
   },
   plugins: [],
 }
