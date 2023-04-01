f69d948983d82a48563df7090503d62195c4a53f
olvr-me <resing.oliver@gmail.com>
2023-04-01 19:11:12 +0200

    Added Helper API function to get song by date

--- lib/api.ts
+++ lib/api.ts
@@ -10,6 +10,26 @@ export const getAllDates = async () => {
     return response.json();
 };
 
+export const getSongByDate = async (date) => {
+    const allDates = await getAllDates();
+
+    let foundSongId;
+    allDates.dates.forEach(d => {
+
+        console.log(`date: ${d.date} searchDate: ${date} are equal: ${d.date === date}`)
+
+        if (d.date === date) {
+            foundSongId = d.songId;
+        }
+    });
+
+    if (foundSongId === undefined) {
+        return null;
+    }
+
+    return await getSong(foundSongId);
+}
+
 export const getSong = async (songId) => {
     const response = await fetch(`http://localhost:3000/api/song?songId=${songId}`, {
         method: "GET",
