7e90181a3e6b92333d3f0d9751acca6e52748d26
olvr-me <resing.oliver@gmail.com>
2023-04-01 15:02:29 +0200

    Added new dates to files

--- app/api/dates/route.ts
+++ app/api/dates/route.ts
@@ -2,6 +2,14 @@ import { NextResponse } from 'next/server';
 
 const songs = [
     {
+        date: '01.04.2023',
+        songId: '6YjVrP2g7gJYh3aXkgnspC'
+    },
+    {
+        date: '31.03.2023',
+        songId: '0hJfuyUwtEYMlGgvr3nzz9'
+    },
+    {
         date: '28.03.2023',
         songId: '1sklC11tCgdL59gNWB7Vli'
     },
