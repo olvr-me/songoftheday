a861a22bf7bf4e2ad229e6f9806a135a72d45769
olvr-me <resing.oliver@gmail.com>
2023-04-03 20:55:40 +0200

    Chaged API to relative path for common and to .env value for specific API-Call

--- lib/api.ts
+++ lib/api.ts
@@ -1,17 +1,20 @@
 export const getAllDates = async () => {
-    const response = await fetch('http://localhost:3000/api/dates', {
+    const response = await fetch(`/api/dates`, {
         method: "GET",
         headers: {
             Accept: "application/json",
             "Content-Type": "application/json",
-        }
+        },
+        cache: 'no-cache'
     });
 
     return response.json();
 };
 
 export const getSongByDate = async (date) => {
-    const response = await fetch(`http://localhost:3000/api/dates?date=${date}`, {
+    const response = await fetch(`${process.env.BASE_URL}/api/dates?` + new URLSearchParams({
+        date: date,
+    }), {
         method: "GET",
         headers: {
             Accept: "application/json",
