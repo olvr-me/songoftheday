cb780332fafa7fa7bc8c90070c7f46bf91020cc1
olvr-me <resing.oliver@gmail.com>
2023-04-08 14:15:02 +0200

    Changed colors for header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 export default function Header() {
     return (
         <div className="flex justify-between items-center mb-1 text-lg">
-            <h1>songoftheday</h1>
+            <h1 className="text-green-400">songoftheday</h1>
             <Link
+                className="text-green-400"
                 href="https://open.spotify.com/playlist/19VvREnkIgYtLATS9s9hFo?si=20ceb02ab6854365">playlist
             </Link>
         </div>
