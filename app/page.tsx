a8847a67503338cae4be4f7155e6300b49fffd9b
olvr-me <resing.oliver@gmail.com>
2023-04-08 14:17:42 +0200

    Changed default text

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ export default async function Page() {
   return (
     <main>
       <div>
-        <h1>Hello,</h1>
-        <p>this is a small collection of songs which I liked in the past.</p>
+        <p>Hello, this is a small collection of songs which I liked in the past.</p>
         <p>Select a date and you can see which one it was on that particular day :)</p>
       </div>
     </main>
