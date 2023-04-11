92242d24049a0d6d17bacac9fd1cbb4103c9c332
olvr-me <resing.oliver@gmail.com>
2023-04-11 19:46:48 +0200

    cleanup of skeleton component for song

--- components/SongSkeleton.tsx
+++ components/SongSkeleton.tsx
@@ -1,8 +1,7 @@
 export default function SongSkeleton() {
     return (
-        <div className="flex justify-center flex-col ml-1 pb-5">
-            <div className="animate-pulse  w-songw h-songh bg-slate-500">
-            </div>
+        <div className="flex justify-center flex-col m-1 pb-5">
+            <div className="animate-pulse w-songw h-songh bg-slate-500" />
             <h3 className="className='text-slate-400'">loading...</h3>
             <h3 className='text-slate-400'>loading...</h3>
         </div>
