3e3f14c22137898c1dddb69fc7960f15745cf573
olvr-me <resing.oliver@gmail.com>
2023-04-12 18:34:40 +0200

    Take first light color instead of the first found color

--- components/Song.tsx
+++ components/Song.tsx
@@ -2,28 +2,62 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import spotifyLogo from '../assets/Spotify_Logo_RGB_Green.png'
-import Color from 'color-thief-react';
-import SongSkeleton from './SongSkeleton';
+import { Palette } from 'color-thief-react';
 
 function Song({ title, artists, imageUrl, link }) {
 
     function getGlowEffect(color) {
 
-        if (color.data === undefined) {
+        if (color === undefined) {
             return {};
         }
 
         return {
-            filter: `drop-shadow(0 0 50px ${color.data}`
+            filter: `drop-shadow(0 0 50px ${color}`
+        }
+    }
+
+    function isColorLight(color) {
+        // Remove "rgb(" and ")" to isolate the color values
+        var colorValues = color.substring(4, color.length - 1)
+            .replace(/ /g, '')
+            .split(',');
+
+        // Convert the color values to an array of integers
+        var r = parseInt(colorValues[0]);
+        var g = parseInt(colorValues[1]);
+        var b = parseInt(colorValues[2]);
+
+        // Calculate the relative luminance of the color
+        var luminance = (0.2126 * r) + (0.7152 * g) + (0.0722 * b);
+
+        // Check if the luminance is greater than or equal to 128
+        const luminanceThreshhold = 80; // This value is just an estimate
+
+        if (luminance >= luminanceThreshhold) {
+            return true; // Color is light
+        } else {
+            return false; // Color is dark
         }
     }
 
     return (
         <div className='flex justify-center flex-col m-1 pb-5'>
-            <Color src={imageUrl} crossOrigin="anonymous" format='rgbString'>
+            <Palette src={imageUrl} crossOrigin='anonymous' format='rgbString'>
                 {({ data, loading }) => {
                     if (loading)
                         return <div className="animate-pulse w-songw h-songh bg-slate-500" />;
+
+                    let lightColor;
+                    data.forEach((color, index) => {
+                        if (isColorLight(color) && lightColor === undefined) {
+                            lightColor = color;
+                        }
+                    });
+
+                    // If there is no light color, we just take the first one
+                    if (lightColor === undefined)
+                        lightColor = data[0];
 
                     return (
                         <Image
@@ -31,11 +65,11 @@ function Song({ title, artists, imageUrl, link }) {
                             width={600}
                             height={600}
                             alt={`Coverimage of song \'${title}\' from artists \'${artists}\'`}
-                            style={getGlowEffect({ data })}
+                            style={getGlowEffect(lightColor)}
                         />
                     );
                 }}
-            </Color>
+            </Palette>
             <div className='mt-5 flex flex-row justify-end'>
                 <div className='basis-2/3'>
                     <h3 className='text-white text-xl'>{title}</h3>
